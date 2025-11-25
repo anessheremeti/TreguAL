88f5f566dea5be56431755ed46b4f56b035012d6
Anes Sheremeti <anessheremeti9@gmail.com>
2025-11-25 10:12:03 +0100

    feat:react router setup

--- TreguAL/src/App.jsx
+++ TreguAL/src/App.jsx
@@ -2,11 +2,15 @@ import "./App.css";
 import React from "react";
 import HomePage from "./pages/HomePage/index";
 import Product from "./pages/Product";
+import {  Routes, Route } from "react-router";
+
 function App() {
   return (
     <>
-      <HomePage />
-      <Product />
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/product" element={<Product />} />
+    </Routes>
     </>
   );
 }
