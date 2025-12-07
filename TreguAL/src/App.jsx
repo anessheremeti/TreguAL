8a9a84d49e7a840c3b42dcfebf9762b90958475c
EnsarZeqiri <ensarzeqiri15@gmail.com>
2025-12-07 11:10:01 +0100

    Create Ensar branch

--- TreguAL/src/App.jsx
+++ TreguAL/src/App.jsx
@@ -4,13 +4,14 @@ import HomePage from "./pages/HomePage/index";
 import Product from "./pages/Product";
 import {  Routes, Route } from "react-router";
 import EShop from "./pages/E-Shop";
+import AddItemScreen from "./pages/AddItemScreen";
 function App() {
   return (
     <>
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/product" element={<Product />} />
-      <Route path="/e-shop" element={<EShop />} />
+      <Route path="/addItemScreen" element={<AddItemScreen />} />
     </Routes>
     </>
   );
