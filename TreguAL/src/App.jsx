334873b290227c8c59d838d648aa6286345596f4
Anes Sheremeti <anessheremeti9@gmail.com>
2025-12-23 22:58:18 +0100

    feat:blog page finished

--- TreguAL/src/App.jsx
+++ TreguAL/src/App.jsx
@@ -11,6 +11,7 @@ import DashboardAdmin from "./pages/DashboardAdmin";
 import MyPostsPage from "./pages/MyPostsList";
 import Blog from "./pages/Blog";
 import AboutUs from "./pages/AboutUs";
+import SignUp from "./pages/SignUp";
 function App() {
   return (
     <>
@@ -26,6 +27,7 @@ function App() {
         <Route path="aboutus" element={<AboutUs />} />
         <Route path="/blog" element={<Blog />} />
         <Route path="/product" element={<Product />} />
+        <Route path="/signup" element={<SignUp />} />
       </Routes>
     </>
   );
